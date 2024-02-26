f76a10d9b0b541068ceffff6f697d0dd616a50ae
MasterDev333 <axe.vldk@gmail.com>
2024-02-26 16:50:24 +1000

    updating

---
+++ assets/js/main.js
@@ -0,0 +1,200 @@
+/* eslint-disable no-undef */
+/* eslint-disable func-names */
+// eslint-disable-next-line func-names
+(function($) {
+  const helper = {
+    // custom helper function for debounce - how to work see https://codepen.io/Hyubert/pen/abZmXjm
+    /**
+     * Debounce
+     * need for once call function
+     *
+     * @param { function } - callback function
+     * @param { number } - timeout time (ms)
+     * @return { function }
+     */
+    debounce(func, timeout) {
+      let timeoutID;
+      // eslint-disable-next-line no-param-reassign
+      timeout = timeout || 200;
+      return function() {
+        const scope = this;
+        // eslint-disable-next-line prefer-rest-params
+        const args = arguments;
+        clearTimeout(timeoutID);
+        timeoutID = setTimeout(function() {
+          func.apply(scope, Array.prototype.slice.call(args));
+        }, timeout);
+      };
+    },
+    /**
+     * Helper if element exist then call function
+     */
+    isElementExist(_el, _cb, _argCb) {
+      const elem = document.querySelector(_el);
+      if (document.body.contains(elem)) {
+        try {
+          if (arguments.length <= 2) {
+            _cb();
+          } else {
+            _cb(..._argCb);
+          }
+        } catch (e) {
+          // eslint-disable-next-line no-console
+          console.log(e);
+        }
+      }
+    },
+
+    /**
+     *  viewportCheckerAnimate function
+     *
+     * @param whatElement - element name
+     * @param whatClassAdded - class name if element is in viewport
+     * @param classAfterAnimate - class name after element animates
+     */
+    viewportCheckerAnimate(whatElement, whatClassAdded, classAfterAnimate) {
+      jQuery(whatElement)
+        .addClass('a-hidden')
+        .viewportChecker({
+          classToRemove: 'a-hidden',
+          classToAdd: `animated ${whatClassAdded}`,
+          offset: 10,
+          callbackFunction(elem) {
+            if (classAfterAnimate) {
+              elem.on('animationend', () => {
+                elem.addClass('animation-end');
+              });
+            }
+          }
+        });
+    },
+    // helpler windowResize
+    windowResize(functName) {
+      const self = this;
+      $(window).on('resize orientationchange', self.debounce(functName, 200));
+    },
+    /**
+     * Init slick slider only on mobile device
+     *
+     * @param {DOM} $slider
+     * @param {array} option - slick slider option
+     */
+    mobileSlider($slider, option) {
+      if (window.matchMedia('(max-width: 768px)').matches) {
+        if (!$slider.hasClass('slick-initialized')) {
+          $slider.slick(option);
+        }
+      } else if ($slider.hasClass('slick-initialized')) {
+        $slider.slick('unslick');
+      }
+    },
+    /**
+     * Set cookie
+     *
+     * @param {string} name
+     * @param {string} value
+     * @param {int} days
+     */
+    setCookie(name, value, days) {
+      var expires = '';
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = '; expires=' + date.toUTCString();
+      }
+      document.cookie = name + '=' + (value || '') + expires + '; path=/';
+    },
+    /**
+     *Get Cookie
+     *
+     * @param {string} name
+     * @return {string}
+     */
+    getCookie(name) {
+      var nameEQ = name + '=';
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+      }
+      return null;
+    },
+    /**
+     * Erase Cookie,
+     *
+     * @param {string} name
+     */
+    eraseCookie(name) {
+      document.cookie =
+        name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
+  };
+
+  const theme = {
+    /**
+     * Main init function
+     */
+    init() {
+      this.plugins(); // Init all plugins
+      this.bindEvents(); // Bind all events
+      this.initAnimations(); // Init all animations
+    },
+
+    /**
+     * Init External Plugins
+     */
+    plugins() {},
+
+    /**
+     * Bind all events here
+     *
+     */
+    bindEvents() {
+      const self = this;
+      /** * Run on Document Ready ** */
+      $(document).on('ready', function() {
+        self.smoothScrollLinks();
+      });
+      /** * Run on Window Load ** */
+      $(window).on('scroll', function() {
+        if ($(window).scrollTop() >= 50)
+          $('.header').addClass('header--sticky');
+        else $('.header').removeClass('header--sticky');
+      });
+    },
+
+    /**
+     * init scroll revealing animations function
+     */
+    initAnimations() {
+      helper.viewportCheckerAnimate('.a-up', 'fadeInUp', true);
+      helper.viewportCheckerAnimate('.a-down', 'fadeInDown');
+      helper.viewportCheckerAnimate('.a-left', 'fadeInLeft');
+      helper.viewportCheckerAnimate('.a-right', 'fadeInRight');
+      helper.viewportCheckerAnimate('.a-op', 'fade');
+    },
+
+    /**
+     * Smooth Scroll link
+     */
+    smoothScrollLinks() {
+      $('a[href^="#"').on('click touchstart', function() {
+        const target = $(this).attr('href');
+        if (target !== '#' && $(target).length > 0) {
+          const offset = $(target).offset().top - $('header').outerHeight();
+          $('html, body').animate(
+            {
+              scrollTop: offset
+            },
+            500
+          );
+        }
+        return false;
+      });
+    }
+  };
+
+  // Initialize Theme
+  theme.init();
+})(jQuery);
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+	<meta name="format-detection" content="telephone=no">
+	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php endif; ?>
+	<link rel="shortcut icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() . '/assets/images/favicon.png' ); ?>">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
